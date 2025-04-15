1593942bd5b227a386d6911e75cd4ae5b43b1953
Lucas Miyasaki <lhmiyasaki@gmail.com>
2025-04-15 16:23:47 -0300

    feat: criação da classe de Objeto3D

---
+++ Objetos3D/Classes/Objeto3D.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos3D.Classes
+{
+    internal class Objeto3D
+    {
+        private List<(float x, float y, float z)> listaVerticesOriginais;
+        private List<(int a, int b, int c)> listaFaces;
+
+        public Objeto3D()
+        {
+            listaVerticesOriginais = new List<(float x, float y, float z)>();
+            listaFaces = new List<(int a, int b, int c)>();
+        }
+
+        public void AddVertice(float x, float y, float z)
+        {
+            listaVerticesOriginais.Add((x, y, z));
+        }
+
+        public List<(float x, float y, float z)> GetListaVerticeOriginais()
+        {
+            return listaVerticesOriginais;
+        }
+
+        public void AddFaces(int x, int y, int z)
+        {
+            listaFaces.Add((x, y, z));
+        }
+
+        public List<(int a, int b, int c)> GetListaFaces()
+        {
+            return listaFaces;
+        }
+    }
+}
