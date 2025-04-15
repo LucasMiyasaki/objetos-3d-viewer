b6a4fa7fb0410d6b60f6c558057e080be7e60163
Lucas Miyasaki <lhmiyasaki@gmail.com>
2025-04-15 16:26:33 -0300

    feat: adicionado metodos para abrir um objeto 3d, visualizar e mudar a escala do objeto

--- Objetos3D/Form1.Designer.cs
+++ Objetos3D/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            btnAbrirObjeto = new Button();
+            nupEscala = new NumericUpDown();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nupEscala).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1143, 806);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnAbrirObjeto
+            // 
+            btnAbrirObjeto.Location = new Point(1206, 68);
+            btnAbrirObjeto.Name = "btnAbrirObjeto";
+            btnAbrirObjeto.Size = new Size(160, 29);
+            btnAbrirObjeto.TabIndex = 1;
+            btnAbrirObjeto.Text = "Abrir Objeto";
+            btnAbrirObjeto.UseVisualStyleBackColor = true;
+            btnAbrirObjeto.Click += btnAbrirObjeto_Click;
+            // 
+            // nupEscala
+            // 
+            nupEscala.DecimalPlaces = 2;
+            nupEscala.Increment = new decimal(new int[] { 5, 0, 0, 131072 });
+            nupEscala.Location = new Point(1216, 223);
+            nupEscala.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            nupEscala.Name = "nupEscala";
+            nupEscala.Size = new Size(150, 27);
+            nupEscala.TabIndex = 2;
+            nupEscala.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            nupEscala.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(1216, 182);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 28);
+            label1.TabIndex = 3;
+            label1.Text = "Escala";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1403, 830);
+            Controls.Add(label1);
+            Controls.Add(nupEscala);
+            Controls.Add(btnAbrirObjeto);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nupEscala).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnAbrirObjeto;
+        private NumericUpDown nupEscala;
+        private Label label1;
     }
 }
--- Objetos3D/Form1.cs
+++ Objetos3D/Form1.cs
@@ -1,10 +1,130 @@
+using Objetos3D.Classes;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+
 namespace Objetos3D
 {
     public partial class Form1 : Form
     {
+        private Objeto3D objeto;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAbrirObjeto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Arquivos OBJ (*.obj)|*.obj";
+            openFileDialog.Title = "Selecione um arquivo .obj";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                reiniciar();
+                string caminhoArquivo = openFileDialog.FileName;
+                lerArquivo(caminhoArquivo);
+                desenhaObjeto();
+            }
+        }
+
+        private void reiniciar()
+        {
+            objeto = new Objeto3D();
+            pictureBox1.Image = null;
+            nupEscala.Value = 1;
+        }
+
+        private void lerArquivo(string caminhoArquivo)
+        {
+            try
+            {
+                string[] linhas = File.ReadAllLines(caminhoArquivo);
+
+                foreach (string linha in linhas)
+                {
+                    if (linha.StartsWith("v ")) // apenas vértices
+                    {
+                        string[] partes = linha.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (partes.Length >= 4)
+                        {
+                            float x, y, z;
+                            bool temVirgula = partes.Skip(1).Take(3).Any(p => p.Contains(","));
+
+                            if (temVirgula)
+                            {
+                                x = float.Parse(partes[1], new CultureInfo("pt-BR"));
+                                y = float.Parse(partes[2], new CultureInfo("pt-BR"));
+                                z = float.Parse(partes[3], new CultureInfo("pt-BR"));
+                            }
+                            else
+                            {
+                                x = float.Parse(partes[1], CultureInfo.InvariantCulture);
+                                y = float.Parse(partes[2], CultureInfo.InvariantCulture);
+                                z = float.Parse(partes[3], CultureInfo.InvariantCulture);
+                            }
+
+                            objeto.AddVertice(x, y, z);
+                        }
+                    }
+                    else if (linha.StartsWith("f ")) // apenas faces
+                    {
+                        string[] partes = linha.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (partes.Length >= 4)
+                        {
+                            int x, y, z;
+
+                            x = int.Parse(partes[1].Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries)[0]);
+                            y = int.Parse(partes[2].Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries)[0]);
+                            z = int.Parse(partes[3].Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries)[0]);
+
+                            objeto.AddFaces(x, y, z);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void desenhaObjeto(float escala = 1)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+
+                int centroX = pictureBox1.Width / 2;
+                int centroY = pictureBox1.Height / 2;
+
+                var vertices = objeto.GetListaVerticeOriginais();
+                var faces = objeto.GetListaFaces();
+
+                foreach (var face in faces)
+                {
+                    var v1 = vertices[face.a - 1]; // .obj começa em 1
+                    var v2 = vertices[face.b - 1];
+                    var v3 = vertices[face.c - 1];
+
+                    Point p1 = new Point((int)(v1.x * escala) + centroX, (int)(-v1.y * escala) + centroY);
+                    Point p2 = new Point((int)(v2.x * escala) + centroX, (int)(-v2.y * escala) + centroY);
+                    Point p3 = new Point((int)(v3.x * escala) + centroX, (int)(-v3.y * escala) + centroY);
+
+                    g.DrawLine(Pens.Black, p1, p2);
+                    g.DrawLine(Pens.Black, p2, p3);
+                    g.DrawLine(Pens.Black, p3, p1);
+                }
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            desenhaObjeto((float)nupEscala.Value);
         }
     }
 }
